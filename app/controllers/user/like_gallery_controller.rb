180b11805e7bad6dbe229446bc5ba26dedebbcf3
dreamcareer7 <dreamcareer070@gmail.com>
2020-10-08 22:16:31 +0530

    added js support for reflecting like icon

--- app/controllers/user/like_gallery_controller.rb
+++ app/controllers/user/like_gallery_controller.rb
@@ -1,10 +1,18 @@
 class User::LikeGalleryController < User::BaseController
-
   def create
     current_user.favorites.create(favoratable_id: params[:favoratable_id], favoratable_type: params[:favoratable_type])
+    item
   end
 
   def destroy
     current_user.favorites.find_by(favoratable_id: params[:favoratable_id], favoratable_type: params[:favoratable_type])&.destroy
+    item
+    render :create
+  end
+
+  private
+
+  def item
+    @item = params[:favoratable_type].constantize.find(params[:favoratable_id])
   end
 end
