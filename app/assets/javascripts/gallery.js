cae920fb499454090132330634e40a77ac911f9f
dreamcareer7 <dreamcareer070@gmail.com>
2020-10-13 18:43:27 +0530

    gallery images

--- app/assets/javascripts/gallery.js
+++ app/assets/javascripts/gallery.js
@@ -2,6 +2,7 @@ $( document ).on('turbolinks:load', function() {
   $('.custom_carousel').slick({
     dots:true,
     centerMode: false,
+    
   });
 
   $(".gallery-category-menu-and-dropdown select").change(function() {
