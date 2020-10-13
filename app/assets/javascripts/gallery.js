d1cb04e463dbd69c595e8417bcf930a2258f91b9
dreamcareer7 <dreamcareer070@gmail.com>
2020-10-13 17:53:52 +0530

    fixes for popup

--- app/assets/javascripts/gallery.js
+++ app/assets/javascripts/gallery.js
@@ -3,4 +3,25 @@ $( document ).on('turbolinks:load', function() {
     dots:true,
     centerMode: false,
   });
+  $("<select />").appendTo(".gallery-category-menu-and-dropdown");
+
+
+         $("<option />", {
+            "selected": "selected",
+            "value"   : "",
+            "text"    : "Select Category..."
+         }).appendTo(".gallery-category-menu-and-dropdown select");
+
+
+         $(".gallery-category-menu-and-dropdown a").each(function() {
+          var el = $(this);
+          $("<option />", {
+              "value"   : el.attr("href"),
+              "text"    : el.text()
+          }).appendTo(".gallery-category-menu-and-dropdown select");
+         });
+
+         $(".gallery-category-menu-and-dropdown select").change(function() {
+           window.location = $(this).find("option:selected").val();
+         });
 })
