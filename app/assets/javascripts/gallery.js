d0293ecf4ed78ab0fd44f7ee9d2428f4b9981c35
dreamcareer7 <dreamcareer070@gmail.com>
2020-10-20 16:31:16 +0530

    handled image rotation

--- app/assets/javascripts/gallery.js
+++ app/assets/javascripts/gallery.js
@@ -2,8 +2,9 @@ $( document ).on('turbolinks:load', function() {
   $('.custom_carousel').slick({
     dots:true,
     centerMode: false,
-    
+    rtl: $("html").hasClass("rtl")
   });
+
 
   $(".gallery-category-menu-and-dropdown select").change(function() {
     window.location = $(this).find("option:selected").val();
