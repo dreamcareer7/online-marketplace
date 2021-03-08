3ea80ff160904a9cd9c4f39beedadc0192e0b3c9
dreamcareer7 <dreamcareer070@gmail.com>
2021-03-08 20:23:32 +0200

    change specilaist name

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,11 +7,10 @@ class HomeController < ApplicationController
     
     @categories = CachedItems.all_categories
     @popular_sub_categories = CachedItems.popular_items(@current_city)
-    my_sp = Category.where(name: "Specialists").first
-    #@specialist_cached_services =CachedItems.cached_sp_services(@specialist)
+    my_sp = Category.where(:name=> "Specialists").first
     @specialist_cached_services =my_sp.services.visible.shuffle.first(16).in_groups_of(4)
     @city_image = @current_city.city_image ? @current_city.city_image : ''
-
+    #@specialist_cached_services =CachedItems.cached_sp_services(@specialist)
     setup_trending_section
   end
 
