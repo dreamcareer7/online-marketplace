9792b62484175b7f5095332c5e70d13994b98655
dreamcareer7 <dreamcareer070@gmail.com>
2021-06-23 01:25:26 +0200

    fix project fix indexing

--- app/controllers/project_feed_controller.rb
+++ app/controllers/project_feed_controller.rb
@@ -83,10 +83,12 @@ class ProjectFeedController < ApplicationController
   def get_projects
     @projects = policy_scope(Project).includes(:translations)
       .not_completed_or_accepted
-      # .by_city(@current_business.cities)
-      # .not_hidden(@current_business.hidden_resources.pluck(:project_id))
-      # .not_applied(@current_business.applied_to_projects.pluck(:project_id))
-      # .approved
+    if @current_business
+      @projects = @projects.by_city(@current_business.cities)
+        .not_hidden(@current_business.hidden_resources.pluck(:project_id))
+        .not_applied(@current_business.applied_to_projects.pluck(:project_id))
+        # .approved
+    end
   end
 
 
